cf8e3d08ceb8c94a5ad0e125642a1e230c15776e
poco2018 <37485109+poco2018@users.noreply.github.com>
2021-12-20 23:01:06 -0500

    Add files via upload

--- Fifth Release/A_easy_Data_Access.py
+++ Fifth Release/A_easy_Data_Access.py
@@ -11,27 +11,33 @@ acc = conn.commands
 act = conn.types
 acu = conn.utilities
 
-id =   '<PROPERTY-7E221F33-829B-4FBC-A670-E74DABCE6289>' # Element ID
-area = "<PROPERTY-AC5CCA52-F79B-4850-92A9-BED7CB7C3847>" # Area
+id =        '<PROPERTY-7E221F33-829B-4FBC-A670-E74DABCE6289>' # Element ID
+area =      "<PROPERTY-AC5CCA52-F79B-4850-92A9-BED7CB7C3847>" # Area
+layer =     "<PROPERTY-25826253-26F2-4D30-BE1B-0FFBB1314903>" #Layer
+perimeter = "<PROPERTY-F2C033C5-7F47-4BF0-BC39-57F4C7E36783>" #Perimeter
+
 text = "Just some text for testing"
 parameters = {}
 parameters["command"] = "GetSelected"
-parameters["inParams"] = ['Some Default \n text is here','3.0']
+parameters["inParams"] = ['API_ZoneID']
 response = acc.ExecuteAddOnCommand(act.AddOnCommandId('AdditionalJSONCommands','Utilities'),parameters)
-#print(response)
+print(response)
 #for xx in response['outParams2']:
    # print(xx)
 #sys.exit()
 val = str(response['outparams2'][0][0])
 #print(val)
 #sys.exit()
+word =  'ID = ' + id +'\n\n' + 'Area: ' + area + "\n\nlayer = " + layer + "\n\nPerimeter: " + perimeter \
++ '\n\nSome Sample Text'
 guid = val
-#print(guid)
-parameters["command"] = "GetComponents"
-parameters["inParams"] = [guid,'API_DoorID','width1']
+#print(guid) 
+parameters["command"] = "SetLabelText"
+parameters["inParams"] = [guid,'API_LabelID',word]
 response = acc.ExecuteAddOnCommand(act.AddOnCommandId('AdditionalJSONCommands','Utilities'),parameters)
-text =''
+#text =''
 print(response)
+sys.exit()
 #text = response['outParams2'][0] # for GetQuantity wall,door,slab,window
 #'''
 #section for calcquanties
@@ -42,7 +48,7 @@ for xx in response['outParams2']:
       text += xx[0] + '\n'
 #'''
 print(text)
-parameters["command"] = "CreateText"
+parameters["command"] = "ChangLabelText"
 parameters["inParams"] = [text,'4.5']
 response = acc.ExecuteAddOnCommand(act.AddOnCommandId('AdditionalJSONCommands','Utilities'),parameters)
 print(response)
